94c52f96c8d06b41a4f1f0644b7f626bf38e8994
mara <mara@97ce8cef-0441-3849-ab0a-48bca6ff4388>
2010-09-30 17:49:11 +0000

    oprava resolvovani url stranky s "/"

--- scripts/php/includes/database.inc.php
+++ scripts/php/includes/database.inc.php
@@ -7,11 +7,11 @@
 		 */		 		 		 		
    define("WEB_DB_HOSTNAME", "localhost");
    define("WEB_DB_USER", "root");
-   define("WEB_DB_PASSWORD", "");
+   define("WEB_DB_PASSWORD", "31root3112");
    //define("WEB_DB_DATABASE", "tmp_wfw_wp");
    //define("WEB_DB_DATABASE", "tmp_wfw_wp_091231");
    //define("WEB_DB_DATABASE", "tmp_wfw_wp_100323");
-   //define("WEB_DB_DATABASE", "tmp_wfw_wp_100817");
-   define("WEB_DB_DATABASE", "tmp_wfw_wp_lfp_100824");
+   //define("WEB_DB_DATABASE", "tmp_wfw_wp_100808");
+   define("WEB_DB_DATABASE", "tmp_wfw_wp_lfp_20100930");
 
 ?>
