42d83eeb762c440a99457f53eac233621c54ed3f
Administrator <Administrator@97ce8cef-0441-3849-ab0a-48bca6ff4388>
2011-09-01 16:16:00 +0000

    Gallery, AdminMenu

---
+++ admin/in/admin-menu.view.php
@@ -0,0 +1,3 @@
+<v:template src="~/templates/in-template.view">
+	<sys:adminMenuItem />
+</v:template>
---
+++ admin/in/edit-admin-menu.view.php
@@ -0,0 +1,5 @@
+<v:template src="~/templates/in-template.view">
+	<v:panel security:requirePerm="CMS.Settings.AdminMenu">
+		<sys:editAdminMenu />
+	</v:panel>
+</v:template>
