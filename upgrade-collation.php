e58a767840f365a394acab6ed6ae41d67920d056
Marek Fišera <mara@neptuo.com>
2017-12-17 10:46:14 +0000

    Complete utf8 upgrade script.

--- upgrade-collation.php
+++ upgrade-collation.php
@@ -8,7 +8,8 @@ require_once("scripts/php/includes/database.inc.php");
 function mylog($message, $data = null) {
     echo $message . '<br />';
     if (isset($data)) {
-        echo '<pre>' . str_replace('<', '&lt;', str_replace('>', '&gt;', var_export($data))) . '</pre>';
+        echo '<pre>';
+        echo str_replace('<', '&lt;', str_replace('>', '&gt;', var_export($data))) . '</pre>';
     }
 }
 
@@ -96,9 +97,132 @@ if ($defaultCharacterSet == $targetCharacterSet) {
 }
 
 $database = array();
-$database['article_content'] = array();
-$database['article_content']['key'] = array('article_id', 'language_id');
-$database['article_content']['columns'] = array('name', 'url', 'keywords', 'head', 'content', 'author', 'datetime');
+
+$database['article_content'] = array(
+    'key' => array('article_id', 'language_id'),
+    'columns' => array('name', 'url', 'keywords', 'head', 'content', 'author', 'datetime')
+);
+$database['article_label'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'url')
+);
+$database['article_label_language'] = array(
+    'key' => array('label_id', 'language_id'),
+    'columns' => array('name', 'url')
+);
+$database['article_line'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'url')
+);
+$database['content'] = array(
+    'key' => array('page_id', 'language_id'),
+    'columns' => array('tag_lib_start', 'tag_lib_end', 'head', 'content')
+);
+$database['customform'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'fields')
+);
+$database['db_connection'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'hostname', 'user', 'password', 'database', 'fs_root')
+);
+$database['directory'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'url')
+);
+$database['embedded_resource'] = array(
+    'key' => array('id'),
+    'columns' => array('type', 'url')
+);
+$database['file'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'title', 'url')
+);
+$database['group'] = array(
+    'key' => array('gid'),
+    'columns' => array('name')
+);
+$database['group_perms'] = array(
+    'key' => array('id'),
+    'columns' => array('name')
+);
+$database['guestbook'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'content')
+);
+$database['info'] = array(
+    'key' => array('page_id', 'language_id'),
+    'columns' => array('name', 'title', 'href', 'keywords')
+);
+$database['inquiry'] = array(
+    'key' => array('id'),
+    'columns' => array('question')
+);
+$database['inquiry_answer'] = array(
+    'key' => array('id'),
+    'columns' => array('answer')
+);
+$database['language'] = array(
+    'key' => array('id'),
+    'columns' => array('language')
+);
+$database['page_file'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'content')
+);
+$database['page_property_value'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'value')
+);
+$database['personal_note'] = array(
+    'key' => array('id'),
+    'columns' => array('value')
+);
+$database['personal_property'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'value')
+);
+$database['system_adminmenu'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'icon', 'perm')
+);
+$database['system_property'] = array(
+    'key' => array('id'),
+    'columns' => array('key', 'value')
+);
+$database['template'] = array(
+    'key' => array('id'),
+    'columns' => array('name', 'content')
+);
+$database['urlcache'] = array(
+    'key' => array('id'),
+    'columns' => array('url', 'domain_url', 'root_url', 'virtual_url', 'pages_id')
+);
+$database['user'] = array(
+    'key' => array('uid'),
+    'columns' => array('name', 'surname', 'login')
+);
+
+$forms = $db->fetchAll('select `id`, `name`, `fields` from `customform` order by `id`;');
+foreach ($forms as $form) {
+    $name = 'cf_' . $form['name'];
+    $database[$name] = array();
+    $database[$name]['key'] = array();
+    $database[$name]['columns'] = array();
+
+    $primaryKeys = $db->fetchAll('SHOW KEYS FROM `' . $name . '` WHERE Key_name = "PRIMARY";');
+    foreach ($primaryKeys as $primaryKey) {
+        $database[$name]['key'][] = $primaryKey['Column_name'];
+    }
+
+    $fields = explode(";", $form['fields']);
+    foreach ($fields as $field) {
+        $fieldDefinition = explode(":", $field);
+        if (strlen($fieldDefinition[0]) > 0) {
+            $database[$name]['columns'][] = $fieldDefinition[0];
+        }
+    }
+}
 
 $isSuccess = true;
 $db->transaction();
