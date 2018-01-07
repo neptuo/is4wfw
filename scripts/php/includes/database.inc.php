d1a1d3f217955f5333f410f3cefdb351c9f73b99
Marek Fišera <mara@neptuo.com>
2018-01-07 20:22:30 +0100

    Fix.

--- scripts/php/includes/database.inc.php
+++ scripts/php/includes/database.inc.php
@@ -6,7 +6,7 @@
  *
  */
 define("WEB_DB_HOSTNAME", "127.0.0.1");
-define("WEB_DB_USER", "neptuo.com");
-define("WEB_DB_PASSWORD", "QTnF85s7");
-define("WEB_DB_DATABASE", "neptuocom1");
+define("WEB_DB_USER", "");
+define("WEB_DB_PASSWORD", "");
+define("WEB_DB_DATABASE", "");
 ?>
