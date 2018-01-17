b98113d3021903a969f63eb2c4099a899031722f
Marek Fišera <mara@neptuo.com>
2018-01-17 21:50:12 +0100

    Complete install.php for easy installing new instance.

--- install.php
+++ install.php
@@ -5,9 +5,13 @@ $targetFilePath = $targetDirectoryPath . '/database.inc.php';
 
 $url = 'https://api.github.com/repos/maraf/PHP-WebFramework/releases';
 
-if (file_exists($targetFilePath)) {
+if (file_exists($targetDirectoryPath)) {
     header("Location: /"); 
     exit;
+}
+
+function mylog($message) {
+    echo $message . "<br />";
 }
 
 function httpGet($url, $binary = false) {
@@ -30,7 +34,7 @@ function httpGet($url, $binary = false) {
         $content = file_get_contents($url);
         return $content;
     } else {
-        echo 'You have neither cUrl installed nor allow_url_fopen activated. Please setup one of those!';
+        mylog('You have neither cUrl installed nor allow_url_fopen activated. Please setup one of those!');
         exit;
     }
 
@@ -47,54 +51,75 @@ function httpGetJson($url) {
     return null;
 }
 
-if (!file_exists($targetDirectoryPath)) {
-    $json = httpGetJson($url);
-    if ($json == null) {
-        echo 'Unnable to get releases.';
-        exit;
-    }
+$targetUrl = $_SERVER['REQUEST_URI'];
+$targetUrl = str_replace("install.php", "setup.php", $targetUrl);
 
-    if ($json->message != null) {
-        echo $json->message;
-        exit;
-    }
-    
-    if (!is_array($json) || $json[0] == null) {
-        echo 'No release found.';
-        exit;
-    }
-    
-    $json = httpGetJson($json[0]->assets_url);
-    if ($json == null) {
-        echo 'Unnable to get assets for latest release.';
-        exit;
-    }
-    
-    foreach ($json as $item) {
-        if ($item->content_type == 'application/x-zip-compressed') {
-            $filename = $item->name;
-    
-            echo $item->browser_download_url;
+if (file_exists($targetDirectoryPath)) {
+    header("Location: " . $targetUrl); 
+    exit;
+}
 
-            // $content = httpGet($item->browser_download_url, true);
-            // file_put_contents($filename, $content);
-            
-            // $zip = new ZipArchive();
-            // if ($zip->open($filename) === TRUE) {
-            //     $zip->extractTo('.');
-            //     $zip->close();
-            // }
-    
-            // unlink($filename);
+mylog("Getting releases from '" . $url . "'.");
 
-            $targetUrl = $_SERVER['REQUEST_URI'];
-            $targetUrl = str_replace("install.php", "setup.php", $targetUrl);
-            header("Location: " . $targetUrl); 
-            break;
-        }
+$json = httpGetJson($url);
+if ($json == null) {
+    mylog("Unnable to get releases.");
+    exit;
+}
+
+if (!is_array($json)) {
+    if (isset($json->message)) {
+        mylog("Remote error: " . $json->message);
+    } else {
+        mylog("No release found.");
     }
 
     exit;
 }
+
+$targetRelease = null;
+foreach ($json as $release) {
+    if (!$release->draft) {
+        $targetRelease = $release;
+        break;
+    }
+}
+
+if ($targetRelease == null) {
+    mylog("Unnable to find release which is not draft.");
+    exit;
+}
+
+mylog("Selected release '" . $targetRelease->tag_name . "' published at '" . $targetRelease->published_at . "' by '" . $targetRelease->author->login . "'.");
+
+foreach ($targetRelease->assets as $asset) {
+    if ($asset->content_type == "application/x-zip-compressed") {
+        mylog("Selected asset '" . $asset->name . "' of size '" . $asset->size . "B'.");
+
+        $filename = $asset->name;
+
+        mylog("Downloading asset from '" . $asset->browser_download_url . "'.");
+
+        if (!file_exists($filename)) {
+            $content = httpGet($asset->browser_download_url, true);
+            file_put_contents($filename, $content);
+        }
+        
+        $zip = new ZipArchive();
+        if ($zip->open($filename) === TRUE) {
+            $zip->extractTo('.');
+            $zip->close();
+        }
+
+        unlink("install.php");
+        unlink($filename);
+        header("Location: " . $targetUrl); 
+        break;
+    } else {
+        mylog("Skipping asset '" . $asset->name . "' of type '" . $asset->content_type . "'.");
+    }
+}
+
+exit;
 
 ?>
