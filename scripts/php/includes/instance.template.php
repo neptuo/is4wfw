555a62f1aaf7cbc53daebbd7ada7c6603b4a378b
Marek Fišera <mara@neptuo.com>
2018-01-18 19:45:49 +0100

    Fix instance WEB_PATH setup.

--- scripts/php/includes/instance.template.php
+++ scripts/php/includes/instance.template.php
@@ -5,7 +5,10 @@ define("WEB_DB_USER", "{database-username}");
 define("WEB_DB_PASSWORD", "{database-password}");
 define("WEB_DB_DATABASE", "{database-database}");
 
-define("WEB_ROOT", "{filesystem-path}");
+define("WEB_ROOT", "/");
+define("WEB_DOCUMENT_ROOT", "{filesystem-path}");
+
+define("WEB_USE_URLCACHE", true);
 
 define("IS_STOPPED", false);
 
--- scripts/php/includes/settings.inc.php
+++ scripts/php/includes/settings.inc.php
@@ -10,8 +10,8 @@
    
    define("FS_ROOT", WEB_ROOT."files/");
    
-   define("WEB_PATH", $_SERVER['DOCUMENT_ROOT'] . WEB_ROOT);
-   define("FS_PATH", $_SERVER['DOCUMENT_ROOT'] . FS_ROOT);
+   define("WEB_PATH", $_SERVER['DOCUMENT_ROOT'] . WEB_DOCUMENT_ROOT . WEB_ROOT);
+   define("FS_PATH", $_SERVER['DOCUMENT_ROOT'] . WEB_DOCUMENT_ROOT . FS_ROOT);
 
    define("FILE_PAGE_PATH", "/file/");
    define("TEMPLATES_CACHE_DIR", "cache/templates/");
@@ -20,8 +20,6 @@
    define("WEB_R_WRITE", 102);
    define("WEB_R_DELETE", 103);
    define("WEB_R_ADDCHILD", 104);
-   
-   define("WEB_USE_URLCACHE", true);
 
    define("WEB_GROUP", "web");
    
