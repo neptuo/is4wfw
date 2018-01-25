e73f45797b627269159075d5cefd9f4d8c02ced1
Marek Fišera <mara@neptuo.com>
2018-01-25 21:04:02 +0100

    A new library for including templates.

---
+++ scripts/php/libs/Template.class.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once("BaseTagLib.class.php");
+require_once("scripts/php/classes/FullTagParser.class.php");
+require_once("scripts/php/classes/LocalizationBundle.class.php");
+
+/**
+ * 
+ *  Class Template. 
+ *      
+ *  @author     maraf
+ *  @timestamp  2018-01-24
+ * 
+ */
+class Template extends BaseTagLib {
+
+    public function __construct() {
+        parent::setTagLibXml("xml/Template.xml");
+    }
+	
+	public function include($id, $params) {
+		$oldParams = parent::request()->get('params', 'template:include');
+		parent::request()->set('params', $params, 'template:include');
+		
+		$return = parent::web()->includeTemplate($id);
+
+		if ($oldParams == null) {
+			parent::request()->clear('template:include');
+		} else {
+			parent::request()->set('params', $oldParams, 'template:include');
+		}
+
+		return $return;
+	}
+
+	public function getProperty($name) {
+		$params = parent::request()->get('params', 'template:include');
+		if ($params != null && array_key_exists($name, $params)) {
+			return $params[$name];
+		}
+
+		return '';
+	}
+}
+
+?>
