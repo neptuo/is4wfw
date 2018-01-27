8efece0ee2353b09d12be36432a2cabdac8e88dd
Marek Fišera <mara@neptuo.com>
2018-01-27 23:11:15 +0100

    Replace tabs by spaces.

--- scripts/php/classes/UrlCache.class.php
+++ scripts/php/classes/UrlCache.class.php
@@ -1,47 +1,47 @@
 <?php
 
-	/**
-	 *
-	 *  Require base tag lib class.
-	 *
-	 */
-	require_once("scripts/php/libs/BaseTagLib.class.php");
+    /**
+     *
+     *  Require base tag lib class.
+     *
+     */
+    require_once("scripts/php/libs/BaseTagLib.class.php");
 
-	/**
-	 *
-	 *	UrlCache
-	 *	DB Table: id, http, https, url, domain_url, root_url, virtual_url, project_id, pages_id, language_id, cachetime, lastcache
-	 *
-	 */
-	class UrlCache extends BaseTagLib {
-	
-		public function __construct() { }
-		
-		public function read($url) {
-			if(WEB_USE_URLCACHE) {
-				return parent::db()->fetchSingle('select `url`, `http`, `https`, `domain_url`, `root_url`, `virtual_url`, `project_id`, `pages_id`, `language_id`, `cachetime`, `lastcache` from `urlcache` where `url` = "'.$url.'";');
-			}
-			return array();
-		}
-		
-		public function write($fullUrl, $webProject, $pages, $lang, $cachetime) {
-			if(WEB_USE_URLCACHE) {
-				$sql = 'insert into `urlcache`(`url`, `http`, `https`, `domain_url`, `root_url`, `virtual_url`, `project_id`, `pages_id`, `language_id`, `cachetime`, `lastcache`) values ("'.$fullUrl.'", '.$webProject['alias']['http'].', '.$webProject['alias']['https'].', "'.$webProject['alias']['domain_url'].'", "'.$webProject['alias']['root_url'].'", "'.$webProject['alias']['virtual_url'].'", '.$webProject['id'].', "'.$pages.'", '.$lang['id'].', '.$cachetime.', '.time().');';
-				parent::db()->execute($sql);
-			}
-		}
-		
-		public function delete($url) {
-			if(WEB_USE_URLCACHE) {
-				parent::db()->execute('delete from `urlcache` where `url` = "'.$url.'";');
-			}
-		}
-		
-		public function updateLastCache($url) {
-			if(WEB_USE_URLCACHE) {
-				parent::db()->execute('update `urlcache` set `lastcache` = '.time().' where `url` = "'.$url.'";');
-			}
-		}
-	}
+    /**
+     *
+     *  UrlCache
+     *  DB Table: id, http, https, url, domain_url, root_url, virtual_url, project_id, pages_id, language_id, cachetime, lastcache
+     *
+     */
+    class UrlCache extends BaseTagLib {
+    
+        public function __construct() { }
+        
+        public function read($url) {
+            if (WEB_USE_URLCACHE) {
+                return parent::db()->fetchSingle('select `url`, `http`, `https`, `domain_url`, `root_url`, `virtual_url`, `project_id`, `pages_id`, `language_id`, `cachetime`, `lastcache` from `urlcache` where `url` = "'.$url.'";');
+            }
+            return array();
+        }
+        
+        public function write($fullUrl, $webProject, $pages, $lang, $cachetime) {
+            if (WEB_USE_URLCACHE) {
+                $sql = 'insert into `urlcache`(`url`, `http`, `https`, `domain_url`, `root_url`, `virtual_url`, `project_id`, `pages_id`, `language_id`, `cachetime`, `lastcache`) values ("'.$fullUrl.'", '.$webProject['alias']['http'].', '.$webProject['alias']['https'].', "'.$webProject['alias']['domain_url'].'", "'.$webProject['alias']['root_url'].'", "'.$webProject['alias']['virtual_url'].'", '.$webProject['id'].', "'.$pages.'", '.$lang['id'].', '.$cachetime.', '.time().');';
+                parent::db()->execute($sql);
+            }
+        }
+        
+        public function delete($url) {
+            if (WEB_USE_URLCACHE) {
+                parent::db()->execute('delete from `urlcache` where `url` = "'.$url.'";');
+            }
+        }
+        
+        public function updateLastCache($url) {
+            if (WEB_USE_URLCACHE) {
+                parent::db()->execute('update `urlcache` set `lastcache` = '.time().' where `url` = "'.$url.'";');
+            }
+        }
+    }
 
 ?>
