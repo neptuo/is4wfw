fac0a0e1b8c40208652396810e92a38351abe0d6
Marek Fišera <mara@neptuo.com>
2018-02-09 13:03:20 +0100

    Upgrade TineMCE. Refactoring resource inclusion. Add czech translation.

--- scripts/js/initTiny.js
+++ scripts/js/initTiny.js
@@ -1,39 +1,51 @@
 /**
  *
- *  @author  Marek Fišera marek.fisera@email.cz
- *  @date    2009/06/18
+ *  @author  mara@neptuo.com
+ *  @date    2018/02/09
  *
  */
-function initTiny(taId) {
-	tinyMCE.init({
-		// General options
-		mode : "none",
-		theme : "advanced",
-		plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+function initTiny(taId, language) {
+	var config = {
+		selector: 'textarea#' + taId,
+		theme: 'modern',
+		plugins: 'code print preview searchreplace autolink visualblocks visualchars fullscreen image link media table hr pagebreak nonbreaking anchor lists textcolor imagetools contextmenu colorpicker help',
+		toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link image | alignleft aligncenter alignright alignjustify | numlist bullist outdent indent | removeformat code',
+		image_advtab: true,
+		suffix: '.min',
+		content_css: [
+			"/wysiwyg-styles.php"
+		]
+	};
 
-		// Theme options
-		theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-		theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code,|,insertdate,inserttime,|,forecolor,backcolor",
-		theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl",
-		//theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,pagebreak",
-		theme_advanced_toolbar_location : "top",
-		theme_advanced_toolbar_align : "left",
-		theme_advanced_statusbar_location : "bottom",
-		theme_advanced_resizing : false,
+	if (typeof(language) != 'undefined' && language != null) {
+		config.language = language;
+	}
 
-		// Example content CSS (should be your site CSS)
-		content_css : "/wysiwyg-styles.php",
+	tinymce.init(config);
 
-		// Drop lists for link/image/media/template dialogs
-		template_external_list_url : "lists/template_list.js",
-		external_link_list_url : "lists/link_list.js",
-		external_image_list_url : "lists/image_list.js",
-		media_external_list_url : "lists/media_list.js",
+	// var config = {
+	// 	// General options
+	// 	mode : "none",
+	// 	theme : "advanced",
+	// 	plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
 
-		// Replace values for the template plugin
-		template_replace_values : {
-			username : "Some User",
-			staffid : "991234"
-		}
-	});
+	// 	// Theme options
+	// 	theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+	// 	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code,|,insertdate,inserttime,|,forecolor,backcolor",
+	// 	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl",
+	// 	//theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,pagebreak",
+	// 	theme_advanced_toolbar_location : "top",
+	// 	theme_advanced_toolbar_align : "left",
+	// 	theme_advanced_statusbar_location : "bottom",
+	// 	theme_advanced_resizing : false,
+
+	// 	// Example content CSS (should be your site CSS)
+	// 	content_css : "/wysiwyg-styles.php",
+
+	// 	// Drop lists for link/image/media/template dialogs
+	// 	template_external_list_url : "lists/template_list.js",
+	// 	external_link_list_url : "lists/link_list.js",
+	// 	external_image_list_url : "lists/image_list.js",
+	// 	media_external_list_url : "lists/media_list.js",
+	// };
 }
--- scripts/php/libs/Js.class.php
+++ scripts/php/libs/Js.class.php
@@ -20,8 +20,33 @@ require_once("scripts/php/classes/ViewHelper.class.php");
  */
 class Js extends BaseTagLib {
 
+    private $includedScripts = array();
+    private $includedStyles = array();
+
     public function __construct() {
         parent::setTagLibXml("xml/Js.xml");
+
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    }
+
+    public function formatScript($path) {
+        if (in_array($path, $this->includedScripts)) {
+            return null;
+        }
+        
+        $this->includedScripts[] = $path;
+        return '<script type="text/javascript" src="' . $path . '?version=' . WEB_VERSION . '"></script>';
+    }
+
+    public function formatStyle($path) {
+        if (in_array($path, $this->includedStyles)) {
+            return null;
+        }
+        
+        $this->includedStyles[] = $path;
+        return '<link rel="stylesheet" href="' . $path . '?version=' . WEB_VERSION . '" type="text/css" />';
     }
 
     /**
@@ -36,60 +61,60 @@ class Js extends BaseTagLib {
 
         if ($useWindows) {
             $return .= ''
-                    . '<link rel="stylesheet" href="~/css/editor.css?version='.WEB_VERSION.'" type="text/css" />'
-                    . '<link rel="stylesheet" href="~/css/edit-area.css?version='.WEB_VERSION.'" type="text/css" />'
-                    . '<link rel="stylesheet" href="~/css/window.css?version='.WEB_VERSION.'" type="text/css" />'
-                    . '<link rel="stylesheet" href="~/css/jquery-autocomplete.css?version='.WEB_VERSION.'" type="text/css" />'
-                    . '<link rel="stylesheet" href="~/css/jquery-wysiwyg.css?version='.WEB_VERSION.'" type="text/css" />'
-                    . '<link rel="stylesheet" href="~/css/demo_table.css?version='.WEB_VERSION.'" type="text/css" />'
-                    . '<script type="text/javascript" src="~/edit_area/edit_area_full.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/jquery/jquery.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/jquery/jquery-autocomplete-pack.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/jquery/jquery-blockui.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/jquery/jquery-dataTables-min.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/jquery/jquery-wysiwyg.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/cookies.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/functions.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/window.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/domready.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/rxmlhttp.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/links.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/processform.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/Closer.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/Confirm.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/Editor.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/FileName.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/CountDown.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/formFieldEffect.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/init.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/tiny-mce/tiny_mce.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/scripts/js/initTiny.js?version='.WEB_VERSION.'"></script>';
+                . self::formatStyle('~/css/editor.css')
+                . self::formatStyle('~/css/edit-area.css')
+                . self::formatStyle('~/css/window.css')
+                . self::formatStyle('~/css/jquery-autocomplete.css')
+                . self::formatStyle('~/css/jquery-wysiwyg.css')
+                . self::formatStyle('~/css/demo_table.css')
+                . self::formatScript('~/edit_area/edit_area_full.js')
+                . self::formatScript('~/js/jquery/jquery.js')
+                . self::formatScript('~/js/jquery/jquery-autocomplete-pack.js')
+                . self::formatScript('~/js/jquery/jquery-blockui.js')
+                . self::formatScript('~/js/jquery/jquery-dataTables-min.js')
+                . self::formatScript('~/js/jquery/jquery-wysiwyg.js')
+                . self::formatScript('~/js/cookies.js')
+                . self::formatScript('~/js/functions.js')
+                . self::formatScript('~/js/window.js')
+                . self::formatScript('~/js/domready.js')
+                . self::formatScript('~/js/rxmlhttp.js')
+                . self::formatScript('~/js/links.js')
+                . self::formatScript('~/js/processform.js')
+                . self::formatScript('~/js/Closer.js')
+                . self::formatScript('~/js/Confirm.js')
+                . self::formatScript('~/js/Editor.js')
+                . self::formatScript('~/js/FileName.js')
+                . self::formatScript('~/js/CountDown.js')
+                . self::formatScript('~/js/formFieldEffect.js')
+                . self::formatScript('~/js/init.js')
+                . self::formatScript('~/tiny-mce/tinymce.min.js')
+                . self::formatScript('~/scripts/js/initTiny.js');
         } else {
             $return .= ''
-                    . '<link rel="stylesheet" href="~/css/cms_nowindows.css?version='.WEB_VERSION.'" type="text/css" />'
-                    . '<link rel="stylesheet" href="~/css/editor.css?version='.WEB_VERSION.'" type="text/css" />'
-                    . '<link rel="stylesheet" href="~/css/edit-area.css?version='.WEB_VERSION.'" type="text/css" />'
-                    . '<link rel="stylesheet" href="~/css/jquery-wysiwyg.css?version='.WEB_VERSION.'" type="text/css" />'
-                    . '<script type="text/javascript" src="~/edit_area/edit_area_full.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/jquery/jquery.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/jquery/jquery-wysiwyg.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/cookies.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/functions.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/domready.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/rxmlhttp.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/links.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/processform.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/Closer.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/Confirm.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/Editor.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/FileName.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/CountDown.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/formFieldEffect.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/js/init_nowindows.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/tiny-mce/tiny_mce.js?version='.WEB_VERSION.'"></script>'
-                    . '<script type="text/javascript" src="~/scripts/js/initTiny.js?version='.WEB_VERSION.'"></script>';
+                . self::formatStyle('~/css/cms_nowindows.css')
+                . self::formatStyle('~/css/editor.css')
+                . self::formatStyle('~/css/edit-area.css')
+                . self::formatStyle('~/css/jquery-wysiwyg.css')
+                . self::formatScript('~/edit_area/edit_area_full.js')
+                . self::formatScript('~/js/jquery/jquery.js')
+                . self::formatScript('~/js/jquery/jquery-wysiwyg.js')
+                . self::formatScript('~/js/cookies.js')
+                . self::formatScript('~/js/functions.js')
+                . self::formatScript('~/js/domready.js')
+                . self::formatScript('~/js/rxmlhttp.js')
+                . self::formatScript('~/js/links.js')
+                . self::formatScript('~/js/processform.js')
+                . self::formatScript('~/js/Closer.js')
+                . self::formatScript('~/js/Confirm.js')
+                . self::formatScript('~/js/Editor.js')
+                . self::formatScript('~/js/FileName.js')
+                . self::formatScript('~/js/CountDown.js')
+                . self::formatScript('~/js/formFieldEffect.js')
+                . self::formatScript('~/js/init_nowindows.js')
+                . self::formatScript('~/tiny-mce/tinymce.min.js')
+                . self::formatScript('~/scripts/js/initTiny.js');
         }
-        if(strpos($_SERVER['REQUEST_URI'], ".view") == -1) {
+        if (strpos($_SERVER['REQUEST_URI'], ".view") == -1) {
             $return = str_replace("~/", UrlResolver::combinePath(WEB_ROOT, UrlResolver::combinePath(UrlResolver::parseScriptRoot($_SERVER['SCRIPT_NAME'], 'file.php'), WEB_ROOT)), $return);
         } else {
             $return = ViewHelper::resolveUrl($return);
@@ -105,11 +130,11 @@ class Js extends BaseTagLib {
             $ajaxMessage = 'Loading ...';
         }
 
-        $return .= '<script type="text/javascript" src="~/js/domready.js?version='.WEB_VERSION.'"></script>';
-        $return .= '<script type="text/javascript" src="~/js/rxmlhttp.js?version='.WEB_VERSION.'"></script>';
-        $return .= '<script type="text/javascript" src="~/js/links.js?version='.WEB_VERSION.'"></script>';
+        $return .= self::formatScript('~/js/domready.js');
+        $return .= self::formatScript('~/js/rxmlhttp.js');
+        $return .= self::formatScript('~/js/links.js');
 
-        $content = file_get_contents("scripts/js/web/ajaxWebInit.js?version='.WEB_VERSION.'");
+        $content = file_get_contents("scripts/js/web/ajaxWebInit.js");
         $content = str_replace("{web-content}", $webContentRootElId, $content);
         $content = str_replace("{root-page}", $rootPageId, $content);
         $content = str_replace("{ajax-message}", $ajaxMessage, $content);
@@ -130,11 +155,13 @@ class Js extends BaseTagLib {
         $namesAsArray = $phpObject->str_tr($names, ',');
         foreach ($namesAsArray as $name) {
             $path = $type == "js" ? "js/" . trim($name) . ".js" : "css/" . trim($name) . ".css";
-            if (file_exists("scripts/" . $path)) {
+            if (file_exists()) {
+                $path = "scripts/" . $path;
+                $virtualPath = '~/' . $path;
                 if ($as == 'inline') {
-                    $return .= ( $type == "js") ? '<script type="text/javascript">' . "\n" . file_get_contents("scripts/" . $path) . "\n" . '</script>' : '<style type="text/css">' . "\n" . file_get_contents("scripts/" . $path) . "\n" . '</style>';
+                    $return .= ($type == "js") ? '<script type="text/javascript">' . "\n" . file_get_contents($path) . "\n" . '</script>' : '<style type="text/css">' . "\n" . file_get_contents($path) . "\n" . '</style>';
                 } else {
-                    $return .= ( $type == "js") ? '<script type="text/javascript" src="~/' . $path . '"></script>' : '<link rel="stylesheet" href="~/' . $path . '" type="text/css" />';
+                    $return .= ($type == "js") ? self::formatScript($path) : self::formatStyle($path);
                 }
             }
         }
@@ -142,18 +169,23 @@ class Js extends BaseTagLib {
         return $return;
     }
 
-    public function tinyMce($ids) {
+    public function tinyMce($ids, $language = '') {
         $return = ''
-        . '<script type="text/javascript" src="~/js/jquery/jquery.js?version='.WEB_VERSION.'"></script>'
-        . '<script type="text/javascript" src="~/tiny-mce/tiny_mce.js?version='.WEB_VERSION.'"></script>'
-        . '<script type="text/javascript" src="~/scripts/js/initTiny.js?version='.WEB_VERSION.'"></script>';
+        . self::formatScript('~/js/jquery/jquery.js')
+        . self::formatScript('~/tiny-mce/tinymce.min.js')
+        . self::formatScript('~/scripts/js/initTiny.js');
 
         $ids = parent::php()->str_tr($ids, ',');
         if (count($ids) > 0) {
             $return .= '<script type="text/javascript"> $(function() { ';
 
             foreach ($ids as $id) {
-                $return .= 'initTiny("' . $id . '"); tinyMCE.execCommand("mceAddControl", true, "' . $id . '"); ';
+                $arguments = '"' . $id . '"';
+                if ($language != '') {
+                    $arguments .= ', "' . $language . '"';
+                }
+
+                $return .= 'initTiny(' . $arguments . ');';
             }
 
             $return .= ' });</script>';
