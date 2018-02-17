5642885e7d6f21d5f5b6e79108613848d1961883
Marek Fišera <mara@neptuo.com>
2018-02-17 19:52:07 +0100

    Remove inquiry selection from admin.

--- admin/in/inquiries.view.php
+++ admin/in/inquiries.view.php
@@ -1,7 +1,5 @@
 ﻿<v:template src="~/templates/in-template.view">
 	<php:register tagPrefix="i" classPath="php.libs.Inquiry" />
-		<i:setCurrentId label="ZŠ" label2="ZUŠ" />
-	
 		<i:editAnswer />
 		<i:listAnswers />
 		<i:edit />
