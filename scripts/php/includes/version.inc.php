1f765ea6949603d99304b1fca2f7ebd24b4ab7da
Marek Fišera <mara@neptuo.com>
2018-03-02 07:35:23 +0100

    Add support for patch version.

--- scripts/php/includes/version.inc.php
+++ scripts/php/includes/version.inc.php
@@ -1,9 +1,10 @@
 <?php
 
 define("BUILD_VERSION", 337);
-define("PREVIEW_VERSION", "");
+define("PATH_VERSION", ""); // .1
+define("PREVIEW_VERSION", ""); // -beta1
 
-define("WEB_VERSION", BUILD_VERSION . PREVIEW_VERSION);
+define("WEB_VERSION", BUILD_VERSION . PATH_VERSION . PREVIEW_VERSION);
 define("CMS_VERSION", "5.06");
 
 ?>
