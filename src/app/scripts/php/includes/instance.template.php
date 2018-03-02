947cf9da7185c0e39707e40fc114a8200f9a25d2
Marek Fišera <mara@neptuo.com>
2018-03-02 08:16:56 +0100

    Refactor instance constants.

--- src/app/scripts/php/includes/instance.template.php
+++ src/app/scripts/php/includes/instance.template.php
@@ -1,15 +1,18 @@
 <?php
 
+// Database connection
 define("WEB_DB_HOSTNAME", "{database-hostname}");
 define("WEB_DB_USER", "{database-username}");
 define("WEB_DB_PASSWORD", "{database-password}");
 define("WEB_DB_DATABASE", "{database-database}");
 
-define("WEB_ROOT", "/");
-define("WEB_DOCUMENT_ROOT", "{filesystem-path}");
+// Instance paths and urls.
+define("DOCUMENT_ROOT", $_SERVER['DOCUMENT_ROOT']);
+define("INSTANCE_PATH", DOCUMENT_ROOT . "{filesystem-path}");
+define("INSTANCE_URL", "/");
 
+// Other instance config
 define("WEB_USE_URLCACHE", true);
-
 define("IS_STOPPED", false);
 
 ?>
