ea7d0405f49b48f3610152b880ec1c6880829a35
Marek Fišera <mara@neptuo.com>
2018-03-17 21:46:05 +0100

    #58 - Add note about manual DB migration need.

--- src/app/admin/in/update.view.php
+++ src/app/admin/in/update.view.php
@@ -2,4 +2,15 @@
 	<web:frame title="Available Updates">
         <sys:versionList />
     </web:frame>
+    <web:frame title="Notes" open="true">
+        <div class="gray-box">
+            If version update contains any DB migration scripts, you must execute them by browsing to:
+
+            <ul>
+                <li>
+                    <web:a pageId="~/migrate.php" text="migrate.php" />
+                </li>
+            </ul>
+        </div>
+    </web:frame>
 </v:template>
