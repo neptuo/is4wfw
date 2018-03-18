5352b20f90e8ebd79c6b77bce888445aec03b563
Marek Fišera <mara@neptuo.com>
2018-03-18 17:17:30 +0100

    #58 - Using intval of DB version.

--- src/app/scripts/php/includes/autoupdate.inc.php
+++ src/app/scripts/php/includes/autoupdate.inc.php
@@ -2,32 +2,35 @@
 
 	require_once(APP_SCRIPTS_PHP_PATH . "classes/manager/SystemProperty.class.php");
 
-	//$dbObject->setMockMode(true);
+	// $dbObject->setMockMode(true);
 
 	$propertyName = "db_version";
 	$property = new SystemProperty($dbObject->getDataAccess());
 
 	// Nacti verzi system a verzi db
-	$databaseVersion = $property->getValue($propertyName);
+	$databaseVersion = intval($property->getValue($propertyName));
 	$newVersion = $databaseVersion;
 
 	// Podpora pouze pro zvysovani verze
 	$xml = new SimpleXMLElement(file_get_contents('data/autoupdate.xml'));
 	foreach ($xml->script as $script) {
 		$attrs = $script->attributes();
-		if ($attrs['build'] > $databaseVersion) {
+
+		$build = intval($attrs['build']);
+		
+		if ($build > $databaseVersion) {
 			$ok = false;
 			$sql = '';
 			if ($attrs['resource'] != '') {
 				// Nacti z souboru
-				//echo 'Resource: "'.$attrs['resource'].'"<br />';
+				// echo 'Resource: "'.$attrs['resource'].'"<br />';
 				if (file_exists($attrs['resource'])) {
 					$ok = true;
 					$sql = file_get_contents($attrs['resource']);
 				}
 			} else {
 				// Pouzij obsah elementu
-				//echo 'Script: "'.$script.'"<br />';
+				// echo 'Script: "'.$script.'"<br />';
 				$ok = true;
 				$sql = trim($script);
 			}
@@ -35,8 +38,8 @@
 			if ($ok) {
 				$dbObject->execute($sql);
 				
-				if ($attrs['build'] > $newVersion) {
-					$newVersion = $attrs['build'];
+				if ($build > $newVersion) {
+					$newVersion = $build;
 				}
 			}
 		}
@@ -46,6 +49,6 @@
 		$property->setValue($propertyName, $newVersion);
 	}
 	
-	//$dbObject->setMockMode(false);
+	// $dbObject->setMockMode(false);
 
 ?>
