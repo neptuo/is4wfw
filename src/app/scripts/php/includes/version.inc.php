296a0ad1a51965489b40deafddeb64a41ee19f02
Marek Fišera <mara@neptuo.com>
2018-09-14 10:17:06 +0200

    Update version info.

--- src/app/scripts/php/includes/version.inc.php
+++ src/app/scripts/php/includes/version.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 define("BUILD_VERSION", 338);
-define("PATCH_VERSION", 5); // .1
+define("PATCH_VERSION", 6); // .1
 define("PREVIEW_VERSION", ""); // -beta1
 
 define("WEB_VERSION", 'v' . BUILD_VERSION . '.' . PATCH_VERSION . PREVIEW_VERSION);
