f24f15bb8cd7d35d57e8abfe3374dc89c8e0045c
Marek Fišera <mara@neptuo.com>
2019-10-04 19:14:49 +0200

    #117 - Support for selecting fields from array in variable declaration.

--- src/app/scripts/php/libs/Variable.class.php
+++ src/app/scripts/php/libs/Variable.class.php
@@ -1,6 +1,7 @@
 <?php
 
 	require_once("BaseTagLib.class.php");
+    require_once(APP_SCRIPTS_PHP_PATH . "classes/ListModel.class.php");
 
 	/**
 	 * 
@@ -16,7 +17,22 @@
 			parent::setTagLibXml("Variable.xml");
 		}
 		
-		public function setValue($name, $value, $scope) {
+		public function setValue($name, $value, $scope, $select = "") {
+			if ($value instanceof ListModel) {
+				$value = $value->items();
+			}
+
+			if (is_array($value)) {
+				if (!empty($select)) {
+					$result = array();
+					foreach ($value as $item) {
+						$result[] = $item[$select];
+					}
+
+					$value = $result;
+				}
+			}
+
 			if ($scope == 'request') {
 				parent::request()->set($name, $value, 'variable');
 			} else if ($scope == 'session') {
