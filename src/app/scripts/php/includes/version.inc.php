d3c74aa88a98a434c49398121969825ace948d39
Marek Fišera <mara@neptuo.com>
2019-10-07 09:59:05 +0200

    Update version info to preview.

--- src/app/scripts/php/includes/version.inc.php
+++ src/app/scripts/php/includes/version.inc.php
@@ -1,8 +1,8 @@
 <?php
 
-define("BUILD_VERSION", 338);
-define("PATCH_VERSION", 10); // .1
-define("PREVIEW_VERSION", ""); // -beta1
+define("BUILD_VERSION", 339);
+define("PATCH_VERSION", 0); // .1
+define("PREVIEW_VERSION", "-preview1"); // -beta1
 
 define("WEB_VERSION", 'v' . BUILD_VERSION . '.' . PATCH_VERSION . PREVIEW_VERSION);
 define("CMS_VERSION", "v5.6");
