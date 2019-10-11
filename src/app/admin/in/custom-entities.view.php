6c8100139326e723e4f79e9bf332056bfd2f547f
Marek Fišera <mara@neptuo.com>
2019-10-11 16:53:56 +0200

    #117 - Localization of admin lists.

--- src/app/admin/in/custom-entities.view.php
+++ src/app/admin/in/custom-entities.view.php
@@ -1,39 +1,45 @@
 <v:template src="~/templates/in-template.view">
-	<web:condition when="post:delete" is="delete">
-		<ced:tableDeleter name="post:table">
-			<web:redirectToSelf />
-		</ced:tableDeleter>
-	</web:condition>
-	<web:condition when="post:ce-manage-create" is="create">
-		<web:frame title="Create">
-			<ced:tableCreator />
-		</web:frame>
-	</web:condition>
-	<web:frame title="Custom Entities">
-		<ced:listTables>
-			<ui:empty items="ced:listTables">
-				<h4 class="warning">No Custom Entities</h4>
-			</ui:empty>
-			<ui:grid items="ced:listTables" class="standart clickable">
-				<ui:column header="Name" value="ced:tableName" />
-				<ui:column header="Description" value="ced:tableDescription" />
+	<loc:use name="customentityadmin">
+		<web:condition when="post:delete" is="delete">
+	        <ced:tableDeleter name="post:table">
+				<web:redirectToSelf />
+			</ced:tableDeleter>
+		</web:condition>
+		<web:condition when="post:create" is="create">
+			<web:frame title="loc:tablelist.create">
+				<ced:tableCreator />
+			</web:frame>
+		</web:condition>
+		<web:frame title="loc:tablelist.title">
+			<ced:listTables>
+				<ui:empty items="ced:listTables">
+					<h4 class="warning">
+						<web:getProperty name="loc:tablelist.nodata" />
+					</h4>
+				</ui:empty>
+				<ui:grid items="ced:listTables" class="standart clickable">
+					<ui:column header="loc:tablelist.name" value="ced:tableName" />
+					<ui:column header="loc:tablelist.description" value="ced:tableDescription" />
 				<ui:columnBoolean header="Audit log" value="ced:tableAuditLog" />
-				<ui:columnTemplate header="">
-					<web:a pageId="~/in/custom-entity-columns.view" param-table="ced:tableName" class="image-button button-edit">
-						<img src="~/images/page_pro.png" />
-					</web:a>
-					<web:a pageId="~/in/custom-entity-localization.view" param-table="ced:tableName" class="image-button">
-						<img src="~/images/lang.png" />
-					</web:a>
-					<admin:deleteButton hiddenField="delete" confirmValue="ced:tableName" hidden-table="ced:tableName" />
-				</ui:columnTemplate>
-			</ui:grid>
-		</ced:listTables>
-		<hr />
-		<ui:form>
-			<div class="gray-box">
-				<button name="ce-manage-create" value="create">Create New Entity</button>
-			</div>
-		</ui:form>
-	</web:frame>
+					<ui:columnTemplate header="">
+						<web:a pageId="~/in/custom-entity-columns.view" param-table="ced:tableName" class="image-button button-edit">
+							<img src="~/images/page_pro.png" />
+						</web:a>
+						<web:a pageId="~/in/custom-entity-localization.view" param-table="ced:tableName" class="image-button">
+							<img src="~/images/lang.png" />
+						</web:a>
+    					<admin:deleteButton hiddenField="delete" confirmValue="ced:tableName" hidden-table="ced:tableName" />
+					</ui:columnTemplate>
+				</ui:grid>
+			</ced:listTables>
+			<hr />
+			<ui:form>
+				<div class="gray-box">
+					<button name="create" value="create">
+						<web:getProperty name="loc:tablelist.create" />
+					</button>
+				</div>
+			</ui:form>
+		</web:frame>
+	</loc:use>
 </v:template>
--- src/app/admin/in/custom-entity-columns.view.php
+++ src/app/admin/in/custom-entity-columns.view.php
@@ -1,35 +1,44 @@
 <v:template src="~/templates/in-template.view">
-	<web:a pageId="~/in/custom-entities.view" text="&laquo; Custom Entities" />
-	<web:condition when="post:delete" is="delete">
-		<ced:tableColumnDeleter tableName="query:table" columnName="post:column">
-			<web:redirectToSelf />
-		</ced:tableColumnDeleter>
-	</web:condition>
-	<web:condition when="post:ce-column-create" is="create">
-		<web:frame title="Create Column">
-			<ced:tableColumnCreator name="query:table" />
+	<loc:use name="customentityadmin">
+		<web:condition when="post:delete" is="delete">
+			<ced:tableColumnDeleter tableName="query:table" columnName="post:column">
+				<web:redirectToSelf />
+			</ced:tableColumDeleter>
+		</web:condition>
+			
+		<utils:concat output="backButtonText" value1="&laquo;" value2=" " value3="loc:tablelist.title" />
+		<web:a pageId="~/in/custom-entities.view" text="utils:backButtonText" />
+		
+		<web:condition when="post:create" is="create">
+			<web:frame title="loc:columnlist.create">
+				<ced:tableColumnCreator name="query:table" />
+			</web:frame>
+		</web:condition>
+
+		<utils:concat output="listTitle" value1="loc:columnlist.title" value2=" :: " value3="query:table" />
+		<web:frame title="utils:listTitle">
+			<ced:listTableColumns name="query:table">
+				<ui:grid items="ced:listTableColumns" class="standart clickable">
+					<ui:column header="loc:columnlist.name" value="ced:tableColumnName" />
+					<ui:column header="loc:columnlist.type" value="ced:tableColumnType" />
+					<ui:columnBoolean header="loc:columnlist.primary" value="ced:tableColumnPrimaryKey" />
+					<ui:columnBoolean header="loc:columnlist.required" value="ced:tableColumnRequired" />
+					<ui:columnBoolean header="loc:columnlist.unique" value="ced:tableColumnUnique" />
+					<ui:columnTemplate header="">
+						<web:condition when="ced:tableColumnPrimaryKey" is="false">
+    						<admin:deleteButton hiddenField="delete" confirmValue="ced:tableColumnName" hidden-column="ced:tableColumnName" />
+						</web:condition>
+					</ui:columnTemplate>
+				</ui:grid>
+			</ced:listTableColumns>
+			<hr />
+			<ui:form>
+				<div class="gray-box">
+					<button name="create" value="create">
+						<web:getProperty name="loc:columnlist.create" />
+					</button>
+				</div>
+			</ui:form>
 		</web:frame>
-	</web:condition>
-	<web:frame title="query:table">
-		<ced:listTableColumns name="query:table">
-			<ui:grid items="ced:listTableColumns" class="standart clickable">
-				<ui:column header="Name" value="ced:tableColumnName" />
-				<ui:column header="Type" value="ced:tableColumnType" />
-				<ui:columnBoolean header="Primary" value="ced:tableColumnPrimaryKey" />
-				<ui:columnBoolean header="Required" value="ced:tableColumnRequired" />
-				<ui:columnBoolean header="Unique" value="ced:tableColumnUnique" />
-				<ui:columnTemplate header="">
-					<web:condition when="ced:tableColumnPrimaryKey" is="false">
-						<admin:deleteButton hiddenField="delete" confirmValue="ced:tableColumnName" hidden-column="ced:tableColumnName" />
-					</web:condition>
-				</ui:columnTemplate>
-			</ui:grid>
-		</ced:listTableColumns>
-		<hr />
-		<ui:form>
-			<div class="gray-box">
-				<button name="ce-column-create" value="create">Create New Column</button>
-			</div>
-		</ui:form>
-	</web:frame>
+	</loc:use>
 </v:template>
