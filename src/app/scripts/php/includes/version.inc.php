057dbd21a572b2069c47b602129b6d468a1bc277
Marek Fišera <mara@neptuo.com>
2019-10-11 08:44:54 +0200

    Update version to preview4.

--- src/app/scripts/php/includes/version.inc.php
+++ src/app/scripts/php/includes/version.inc.php
@@ -2,7 +2,7 @@
 
 define("BUILD_VERSION", 339);
 define("PATCH_VERSION", 0); // .1
-define("PREVIEW_VERSION", "-preview3"); // -beta1
+define("PREVIEW_VERSION", "-preview4"); // -beta1
 
 define("WEB_VERSION", 'v' . BUILD_VERSION . '.' . PATCH_VERSION . PREVIEW_VERSION);
 define("CMS_VERSION", "v5.6");
