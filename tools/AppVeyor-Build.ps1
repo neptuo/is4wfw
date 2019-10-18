81833b34d580ccaa58a3fc026da0a4db89117fd8
Marek Fišera <mara@neptuo.com>
2019-10-18 23:02:37 +0200

    #140 - Better setup for major, patch and preview releases.

--- tools/AppVeyor-Build.ps1
+++
@@ -1,8 +0,0 @@
-$isTag = 'true' -eq $env:APPVEYOR_REPO_TAG;
-if ($isTag) {
-    $source = Invoke-Expression "git describe --abbrev=0 --tags HEAD~1";
-    $target = $env:APPVEYOR_REPO_TAG_NAME;
-    Write-Host "Running an Appveyor release build (tag '$target', patch source '$source').";
-
-    Invoke-Expression ((Join-Path -Path $PSScriptRoot -ChildPath "Pack.ps1") + " $source $target");
-}
