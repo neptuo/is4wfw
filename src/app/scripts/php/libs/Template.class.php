b0aaf3f229d325e0d6e38d3ad7e5cc0a5ccdcd2d
Marek Fišera <mara@neptuo.com>
2020-06-22 09:21:02 +0200

    #26 - Include template by identifier (using any tag).

--- src/app/scripts/php/libs/Template.class.php
+++ src/app/scripts/php/libs/Template.class.php
@@ -17,23 +17,45 @@
 		public function __construct() {
 			parent::setTagLibXml("Template.xml");
 		}
+
+		private function include($filter, $template, $params) {
+			$sql = parent::sql()->select("template", ["id", "content"], $filter);
+			$data = parent::db()->fetchSingle($sql);
+			if (empty($data)) {
+				throw new Error("Missing template filterd by '" . http_build_query($filter) . "'.");
+			}
+
+			if (RoleHelper::isInRole(parent::login()->getGroupsIds(), RoleHelper::getRights(DefaultWeb::$TemplateRightDesc, $data["id"], WEB_R_READ))) {
+				$oldContent = parent::request()->get('content', 'template:include');
+				$oldParams = parent::request()->get('params', 'template:include');
+				parent::request()->set('params', $params, 'template:include');
+				parent::request()->set('content', $template, 'template:include');
+
+				$result = parent::parseContent($data["content"]);
+				
+				parent::request()->set('params', $oldParams, 'template:include');
+				parent::request()->set('content', $oldContent, 'template:include');
+
+				return $result;
+			}
+
+			throw new Error("Permission denied when reading template id = '" . $data["id"] . "'.");
+		}
 		
 		public function includeById($id, $params) {
 			return $this->includeWithBodyById(null, $id, $params);
 		}
 		
 		public function includeWithBodyById($template, $id, $params) {
-			$oldContent = parent::request()->get('content', 'template:include');
-			$oldParams = parent::request()->get('params', 'template:include');
-			parent::request()->set('params', $params, 'template:include');
-			parent::request()->set('content', $template, 'template:include');
-			
-			$return = parent::web()->includeTemplate($id);
-			
-			parent::request()->set('params', $oldParams, 'template:include');
-			parent::request()->set('content', $oldContent, 'template:include');
-			
-			return $return;
+			return $this->include(["id" => $id], $template, $params);
+		}
+
+		public function includeByIdentifier($identifier, $params) {
+			return $this->includeWithBodyByIdentifier($identifier, null, $params);
+		}
+
+		public function includeWithBodyByIdentifier($identifier, $template, $params) {
+			return $this->include(["identifier" => $identifier], $template, $params);
 		}
 
 		public function content() {
