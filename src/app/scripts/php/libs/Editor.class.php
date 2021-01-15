16272626f1985be0bf8ba874d82366f3c87a0b9a
Marek Fišera <mara@neptuo.com>
2021-01-16 00:07:04 +0100

    #269 - Tag edit:execute to processing action like form submit but without user submitting form.

--- src/app/scripts/php/libs/Editor.class.php
+++ src/app/scripts/php/libs/Editor.class.php
@@ -15,7 +15,19 @@
 
 		public function __construct() {
 			parent::setTagLibXml("Editor.xml");
-		}
+        }
+        
+        private function executeSave($template, EditModel $model) {
+            parent::dataAccess()->transaction(function() use ($model, $template) {
+                $model->save();
+                $template();
+                $model->save(false);
+
+                if ($model->hasException()) {
+                    throw new Exception("One or more exceptions raised during save phase of EditModel. All of them should be logged.");
+                }
+            });
+        }
 		
 		public function form($template, $submit, $isEditable = true, $params = []) {
             $model = new EditModel();
@@ -35,15 +47,7 @@
 
                 if ($model->isValid()) {
                     // Save data in transaction.
-                    parent::dataAccess()->transaction(function() use ($model, $template) {
-                        $model->save();
-                        $template();
-                        $model->save(false);
-
-                        if ($model->hasException()) {
-                            throw new Exception("One or more exceptions raised during save phase of EditModel. All of them should be logged.");
-                        }
-                    });
+                    $this->executeSave($template, $model);
 
                     // Process after save redirects.
                     $model->saved(true);
@@ -57,6 +61,28 @@
             $result = parent::ui()->form($template, "post", null, $isEditable, $model->metadata("form"));
             parent::clearEditModel();
             return $result;
+        }
+
+        public function execute($template) {
+            $model = new EditModel();
+            parent::setEditModel($model);
+
+            // Submit form / bind data into the model.
+            $model->submit();
+            $template();
+            $model->submit(false);
+
+            if ($model->isValid()) {
+                // Save data in transaction.
+                $this->executeSave($template, $model);
+
+                // Process after save redirects.
+                $model->saved(true);
+                $template();
+                $model->saved(false);
+            }
+
+            parent::clearEditModel();
         }
 
         public function prefix($template, $name) {
