2108dd2c54db377f5fe112909400feff3884fff0
Marek Fišera <mara@neptuo.com>
2021-04-15 20:26:02 +0200

    #308 - Fix file ordering in root directory. Fix action button line wrapping.

--- src/app/admin/in/filesng.view.php
+++ src/app/admin/in/filesng.view.php
@@ -1,8 +1,13 @@
 <v:template src="~/templates/in-template.view">
 	<loc:use name="fileadmin">
+		<var:declare name="dirId" value="post:dir-id" />
+		<web:condition when="var:dirId" isInverted="true">
+			<var:declare name="dirId" value="0" />
+		</web:condition>
+
 
 		<web:condition when="post:new-import">
-			<fa:importFileSystem dirId="post:dir-id" />
+			<fa:importFileSystem dirId="var:dirId" />
 		</web:condition>
 		<web:condition when="post:file-swap-order">
 			<fa:fileSwapOrder id1="post:file1-id" id2="post:file2-id" />
@@ -14,21 +19,21 @@
 		<fa:upload />
 		<fa:directoryEditor />
 
-		<fa:directoryPath dirId="post:dir-id" separator=" / ">
+		<fa:directoryPath dirId="var:dirId" separator=" / ">
 			<utils:concat output="title" value1="File browser" value2=" :: " value3="fa:directoryPath" />
 			<web:frame title="utils:title">
 				<div class="gray-box">
 					<ui:form>
-						<input type="hidden" name="dir-id" value="<web:getProperty name="post:dir-id" />" />
-						<input type="submit" name="new-file" value="<web:getProperty name="loc:button.newfile" />" />
-						<input type="submit" name="new-directory" value="<web:getProperty name="loc:button.newdirectory" />" />
+						<input type="hidden" name="dir-id" value="<web:out text="var:dirId" />" />
+						<input type="submit" name="new-file" value="<web:out text="loc:button.newfile" />" />
+						<input type="submit" name="new-directory" value="<web:out text="loc:button.newdirectory" />" />
 						|
-						<input type="submit" name="new-zipfile" value="<web:getProperty name="loc:button.newzipfile" />" class="new-zipfile" />
-						<input type="submit" name="new-import" value="<web:getProperty name="loc:button.import" />" class="confirm new-import" />
+						<input type="submit" name="new-zipfile" value="<web:out text="loc:button.newzipfile" />" class="new-zipfile" />
+						<input type="submit" name="new-import" value="<web:out text="loc:button.import" />" class="confirm new-import" />
 					</ui:form>
 				</div>
 
-				<fa:browser dirId="post:dir-id" orderBy="order" parentName="..">
+				<fa:browser dirId="var:dirId" orderBy="order" parentName="..">
 					<ui:grid items="fa:browserList" class="dir-list standart">
 						<utils:concat output="iconClass" value1="file-icon" value2=" " value3="fa:browserExtension" />
 						<var:declare name="iconClass" value="utils:iconClass" />
@@ -41,7 +46,7 @@
 						</ui:columnTemplate>
 						<ui:columnTemplate header="Id" th-class="w30 th-id">
 							<web:condition when="fa:browserId" is="0" isInverted="true">
-								<web:getProperty name="fa:browserId" />
+								<web:out text="fa:browserId" />
 							</web:condition>
 						</ui:columnTemplate>
 
@@ -51,31 +56,31 @@
 
 								</span>
 								<ui:form>
-									<input type="hidden" name="dir-id" value="<web:getProperty name="post:dir-id" />" />
+									<input type="hidden" name="dir-id" value="<web:out text="var:dirId" />" />
 									<web:condition when="fa:browserType" is="0">
-										<input type="hidden" name="directory-id" value="<web:getProperty name="fa:browserId" />">
+										<input type="hidden" name="directory-id" value="<web:out text="fa:browserId" />">
 										<input type="hidden" name="edit-dir" value="Edit">
 										<input type="image" src="/images/page_edi.png" name="edit-dir" value="Edit" title="Edit directory" class="">
 									</web:condition>
 									<web:condition when="fa:browserType" is="0" isInverted="true">
-										<input type="hidden" name="file-id" value="<web:getProperty name="fa:browserId" />" />
+										<input type="hidden" name="file-id" value="<web:out text="fa:browserId" />" />
 										<input type="hidden" name="edit-file" value="Edit" />
 										<input type="image" src="/images/page_edi.png" name="edit-file" value="Edit" title="Edit file" />
 									</web:condition>
 								</ui:form>
 								<ui:form>
-									<input type="hidden" name="dir-id" value="<web:getProperty name="post:dir-id" />" />
+									<input type="hidden" name="dir-id" value="<web:out text="var:dirId" />" />
 									<web:condition when="fa:browserType" is="0">
-										<input type="hidden" name="directory-id" value="<web:getProperty name="fa:browserId" />" />
+										<input type="hidden" name="directory-id" value="<web:out text="fa:browserId" />" />
 										<input type="hidden" name="delete-dir" value="Delete" />
 										<utils:concat output="deleteMessage" value1="Delete directory, id(" value2="fa:browserId" value3=")" />
-										<input class="confirm" type="image" src="/images/page_del.png" name="delete-dir" value="Delete" title="<web:getProperty name="utils:deleteMessage" />" />
+										<input class="confirm" type="image" src="/images/page_del.png" name="delete-dir" value="Delete" title="<web:out text="utils:deleteMessage" />" />
 									</web:condition>
 									<web:condition when="fa:browserType" is="0" isInverted="true">
-										<input type="hidden" name="file-id" value="<web:getProperty name="fa:browserId" />" />
+										<input type="hidden" name="file-id" value="<web:out text="fa:browserId" />" />
 										<input type="hidden" name="delete-file" value="Delete" />
 										<utils:concat output="deleteMessage" value1="Delete file, id(" value2="fa:browserId" value3=")" />
-										<input class="confirm" type="image" src="/images/page_del.png" name="delete-file" value="Delete" title="<web:getProperty name="utils:deleteMessage" />" />
+										<input class="confirm" type="image" src="/images/page_del.png" name="delete-file" value="Delete" title="<web:out text="utils:deleteMessage" />" />
 									</web:condition>
 								</ui:form>
 							</web:condition>
@@ -84,8 +89,8 @@
 						<ui:columnTemplate header="Name" th-class="th-name">
 							<web:condition when="fa:browserType" is="0">
 								<ui:form>
-									<input type="hidden" name="dir-id" value="<web:getProperty name="fa:browserId" />" />
-									<input type="submit" name="ch-dir" value="<web:getProperty name="fa:browserName" />" />
+									<input type="hidden" name="dir-id" value="<web:out text="fa:browserId" />" />
+									<input type="submit" name="ch-dir" value="<web:out text="fa:browserName" />" />
 								</ui:form>
 							</web:condition>
 							<web:condition when="fa:browserType" is="0" isInverted="true">
@@ -130,7 +135,7 @@
 			var $item = $(this);
 			var $placeholder = $item.find(".action-placeholder");
 
-			var dirId = <web:getProperty name="post:dir-id" />;
+			var dirId = <web:out text="var:dirId" />;
 			var html = '';
 			var cssClass = [];
 			if (i > 0) {
