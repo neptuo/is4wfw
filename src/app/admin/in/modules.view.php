9b7b6823e4c56f9b2b9ea6932a84a58ae345a367
Marek Fišera <mara@neptuo.com>
2021-07-14 18:50:53 +0200

    #331 - Basic list of installed modules (styles).

--- src/app/admin/in/modules.view.php
+++ src/app/admin/in/modules.view.php
@@ -1,14 +1,16 @@
 <v:template src="~/templates/in-template.view">
-    <bs:card title="Modules">
-        <module:list>
-            <ui:grid class="table table-striped" thead-class="table-dark" items="module:list">
-                <ui:column header="Alias" value="module:alias" />
-                <ui:column header="Id" value="module:id" />
-                <ui:column header="Name" value="module:name" />
-                <ui:columnTemplate>
-                    <fa5:icon name="trash-alt" class="text-danger" />
-                </ui:columnTemplate>
-            </ui:grid>
-        </module:list>
-    </bs:card>
+    <div class="m-md-2 m-lg-4">
+        <bs:card title="Modules">
+            <module:list>
+                <ui:grid class="table table-striped" thead-class="table-dark" items="module:list">
+                    <ui:column header="Alias" value="module:alias" />
+                    <ui:column header="Id" value="module:id" />
+                    <ui:column header="Name" value="module:name" />
+                    <ui:columnTemplate>
+                        <fa5:icon name="trash-alt" class="text-danger" />
+                    </ui:columnTemplate>
+                </ui:grid>
+            </module:list>
+        </bs:card>
+    </div>
 </v:template>
