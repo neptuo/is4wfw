ab4b5da68b493a3ca726839a267fb3f0467b6e72
Marek Fišera <mara@neptuo.com>
2022-01-06 10:12:51 +0100

    #297 - Load preinit from user folder.

--- src/app/index.php
+++ src/app/index.php
@@ -12,6 +12,10 @@
     require_once(APP_SCRIPTS_PHP_PATH . "includes/version.inc.php");
     require_once(APP_SCRIPTS_PHP_PATH . "includes/extensions.inc.php");
 
+    $userCors = "../user/preinit.inc.php";
+    if (file_exists($userCors)) {
+        require_once($userCors);
+    }
 
     if (array_key_exists("WEB_PAGE_PATH", $_GET)) {
         // Public web.
