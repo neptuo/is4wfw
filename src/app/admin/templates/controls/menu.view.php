b31d3a34bf61ccfcb12d04324d029ba56a9c8657
Marek Fišera <mara@neptuo.com>
2022-01-30 20:59:49 +0100

    #420 - Small redesign of main menu.

--- src/app/admin/templates/controls/menu.view.php
+++ src/app/admin/templates/controls/menu.view.php
@@ -10,16 +10,23 @@
 </web:condition>
 <v:panel class="utils:menuContainerCssClass" data-menu="template:name" security:requirePerm="template:permission"> 
     <div class="menu-root">
-        <web:a pageId="template:rootUrl" title="template:title">
+        <web:a pageId="template:rootUrl" title="template:title" class="btn btn-primary btn-block">
             <web:condition when="template:icon">
-                <img src="<web:out text="template:icon" />" />
+                <var:declare name="iconPrefix" value="fa" />
+                <web:condition when="template:iconPrefix">
+                    <var:declare name="iconPrefix" value="template:iconPrefix" />
+                </web:condition>
+                <fa5:icon prefix="var:iconPrefix" name="template:icon" />
             </web:condition>
-            <span>
+            <span class="text">
                 <web:out text="template:title" />
             </span>
         </web:a>
         <web:condition when="template:toggler" is="" isInverted="true">
-            <button class="menu-toggler" title="<web:out text="utils:menuContainerTogglerTitle" />"></button>
+            <button class="menu-toggler" title="<web:out text="utils:menuContainerTogglerTitle" />">
+                <fa5:icon name="chevron-down" class="menu-toggler-expand" />
+                <fa5:icon name="chevron-up" class="menu-toggler-collapse" />
+            </button>
         </web:condition>
     </div>
     <web:condition when="template:menu">
@@ -29,11 +36,11 @@
                     <web:out security:requirePerm="list:menu-perm">
                         <li>
                             <div class="link">
-                                <web:a pageId="list:menu-url" title="list:menu-text">
+                                <web:a pageId="list:menu-url" title="list:menu-text" class="rounded">
                                     <web:condition when="list:menu-icon">
-                                        <img src="<web:out text="list:menu-icon" />" />
+                                        <img src="<web:out text="list:menu-icon" />" width="16" height="16" />
                                     </web:condition>
-                                    <span>
+                                    <span class="text">
                                         <web:out text="list:menu-text" />
                                     </span>
                                 </web:a>
