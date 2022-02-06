02598523c5353ccf6b40738ce03bdbcd22091608
Marek Fišera <mara@neptuo.com>
2022-02-06 17:35:06 +0100

    #430 - Rename search "subtype" and "location" with "field".

--- src/app/admin/views/search.view.php
+++ src/app/admin/views/search.view.php
@@ -1,20 +1,20 @@
 <web:frame title="Search">
-    <utils:arrayItem output="location" key-id="page-tlstart" key-name="Page (tag lib start)" />
-    <utils:arrayItem output="location" key-id="page-tlend" key-name="Page (tag lib end)" />
-    <utils:arrayItem output="location" key-id="page-head" key-name="Page (head)" />
-    <utils:arrayItem output="location" key-id="page-content" key-name="Page (content)" />
-    <utils:arrayItem output="location" key-id="template-content" key-name="Template (content)" />
-    <utils:arrayItem output="location" key-id="textfile-content" key-name="Text File (content)" />
+    <utils:arrayItem output="field" key-id="page-tlstart" key-name="Page (tag lib start)" />
+    <utils:arrayItem output="field" key-id="page-tlend" key-name="Page (tag lib end)" />
+    <utils:arrayItem output="field" key-id="page-head" key-name="Page (head)" />
+    <utils:arrayItem output="field" key-id="page-content" key-name="Page (content)" />
+    <utils:arrayItem output="field" key-id="template-content" key-name="Template (content)" />
+    <utils:arrayItem output="field" key-id="textfile-content" key-name="Text File (content)" />
 
     <edit:form submit="search">
-        <ui:filter session="location">
+        <ui:filter session="field">
             <div class="gray-box">
                 <label for="query" class="w100">Text:</label>
                 <ui:textbox name="q" id="query" class="w500" />
             </div>
             <div class="gray-box">
-                <label class="w100">Location:</label>
-                <ui:checkboxlist name="location" source="utils:location" display="name" value="id" repeat="horizontal" class="mr-1" />
+                <label class="w100">Field:</label>
+                <ui:checkboxlist name="field" source="utils:field" display="name" value="id" repeat="horizontal" class="mr-1" />
             </div>
             <div class="gray-box">
                 <button name="search">Search</button>
@@ -23,13 +23,13 @@
     </edit:form>
     <hr />
     <web:condition when="query:q">
-        <p:search text="query:q" location="session:location">
+        <p:search text="query:q" location="session:field">
             <ui:empty items="p:searchList">
                 <h4 class="warning">Nothing has been found.</h4>
             </ui:empty> 
             <ui:grid items="p:searchList" class="data-table standart wmax">
                 <ui:column header="Type" th-class="w50" value="p:searchType" />
-                <ui:column header="Subtype" th-class="w40" value="p:searchSubType" />
+                <ui:column header="Field" th-class="w40" value="p:searchSubType" />
                 <ui:column header="Id" th-class="w30" td-class="id" value="p:searchId" />
                 <ui:column header="Name" td-class="mw200 ellipsis" td-title="p:searchName" value="p:searchName" />
                 <ui:columnTemplate header="Content" td-class="file-content">
