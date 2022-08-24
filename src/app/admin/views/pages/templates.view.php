adfccd2521f7132ad5efa63e1688639a58f65e96
Marek Fišera <mara@neptuo.com>
2022-08-24 16:57:32 +0200

    #248 - Clear saved position for opening different template. Check editor exists. Resize on resize.

--- src/app/admin/views/pages/templates.view.php
+++ src/app/admin/views/pages/templates.view.php
@@ -7,29 +7,36 @@
 <js:script placement="tail">
 	const textarea = document.getElementById("template-edit-detail-content");
 	const container = document.getElementById("template-edit-detail-editor");
+	if (textarea) {
+		require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.34.0/min/vs' } });
+		require(['vs/editor/editor.main'], function() {
+			let editor = monaco.editor.create(container, {
+				value: textarea.value,
+				scrollBeyondLastLine: false,
+				language: "html"
+			});
+			editor.focus();
+			
+			const editorKey = textarea.id + "editorSelection";
 
-	require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.34.0/min/vs' } });
-	require(['vs/editor/editor.main'], function() {
-		let editor = monaco.editor.create(container, {
-			value: textarea.value,
-			scrollBeyondLastLine: false,
-			language: "html"
+			const selectionJson = localStorage.getItem(editorKey);
+			if (selectionJson) {
+				const selection = JSON.parse(selectionJson);
+				editor.setSelection(selection);
+
+				localStorage.removeItem(editorKey);
+			}
+
+			window.addEventListener("resize", () => {
+				editor.layout();
+			});
+
+			textarea.form.addEventListener("submit", e => {
+				const selection = editor.getSelection();
+				localStorage.setItem(editorKey, JSON.stringify(selection));
+
+				textarea.value = editor.getValue();
+			});
 		});
-		editor.focus();
-		
-		const editorKey = textarea.id + "editorSelection";
-
-		const selectionJson = localStorage.getItem(editorKey);
-		if (selectionJson) {
-			const selection = JSON.parse(selectionJson);
-			editor.setSelection(selection);
-		}
-
-		textarea.form.addEventListener("submit", e => {
-			const selection = editor.getSelection();
-			localStorage.setItem(editorKey, JSON.stringify(selection));
-
-			textarea.value = editor.getValue();
-		});
-	});
+	}
 </js:script>
