b64f9e7f5daaf17595ed6d821c2daf198270e872
Marek Fišera <mara@neptuo.com>
2022-08-26 14:24:13 +0200

    #248 - Add list of properties to personal properties page.

--- src/app/admin/views/pages/personalProperties.view.php
+++ src/app/admin/views/pages/personalProperties.view.php
@@ -1,2 +1,5 @@
 <web:a pageId="route:defaultProperties" text="&laquo; Default properties" />
 <sys:manageProperties userId="current" />
+<php:using prefix="hint" class="php.libs.Hint">
+	<hint:propertyList />
+</php:using>
