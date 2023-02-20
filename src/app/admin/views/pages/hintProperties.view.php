585c934a5baef054313063b10cbf099ffd336df2
Marek Fišera <mara@neptuo.com>
2023-02-20 15:04:43 +0100

    #495 - Point all documentation links to new documentation.

--- src/app/admin/views/pages/hintProperties.view.php
+++ src/app/admin/views/pages/hintProperties.view.php
@@ -1,4 +1,5 @@
-<web:a pageId="route:hint">&laquo; TagLib manual</web:a>
+<web:a pageId="route:docs">&laquo; Libraries documentation</web:a>
+(<web:a pageId="route:hint">old</web:a>)
 <php:using prefix="hint" class="php.libs.Hint">
 	<hint:propertyList />
 	<hint:autoRegistered />
