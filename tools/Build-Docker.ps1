aac74e6946d783a893d5a529e1eb67d71330f0f1
Marek Fišera <mara@neptuo.com>
2023-03-21 14:47:37 +0100

    #501 - Cleanup index after docker build.

--- tools/Build-Docker.ps1
+++ tools/Build-Docker.ps1
@@ -13,6 +13,19 @@ if ($null -eq $versionName)
 
 Push-Location $PSScriptRoot;
 
+# Backup files to rewrite
+$VersionSourceFile = "..\src\app\scripts\php\includes\version.inc.php"
+$VersionTempFile = New-TemporaryFile
+$SettingsSourceFile = "..\src\app\scripts\php\includes\settings.inc.php"
+$SettingsTempFile = New-TemporaryFile
+$HtaccessSourceFile = "..\src\.htaccess"
+$HtaccessTempFile = New-TemporaryFile
+
+Copy-Item $VersionSourceFile -Destination $VersionTempFile
+Copy-Item $SettingsSourceFile -Destination $SettingsTempFile
+Copy-Item $HtaccessSourceFile -Destination $HtaccessTempFile
+
+# Rewrite various files for docker
 Invoke-Expression ".\Update-Version.ps1 $versionName";
 Invoke-Expression ".\Update-InstancePath.ps1 instance";
 Invoke-Expression ".\DockerizeHtaccess.ps1";
@@ -26,6 +39,11 @@ Invoke-Expression "docker build .. -f ..\docker\is4wfw\dockerfile -t $tag";
 Invoke-Expression "docker build .. -f ..\docker\is4wfw-dev\dockerfile -t $tagDev --build-arg baseversion=$versionName";
 
 Write-Host "Docker image built and tagged as '$tag' and '$tagDev'.";
+
+# Restore backup
+Copy-Item $VersionTempFile -Destination $VersionSourceFile
+Copy-Item $SettingsTempFile -Destination $SettingsSourceFile
+Copy-Item $HtaccessTempFile -Destination $HtaccessSourceFile
 
 if ($push) {
     Invoke-Expression "docker push $tag";
